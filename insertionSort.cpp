c9fde55206ab9c85bb65c037ce31512c89c39d63
csyang79 <csyang79@gmail.com>
2018-04-20 23:07:51 +0800

    update insertionSort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,9 +1,17 @@
+#include <functional>
 #include <iostream>
 #include <vector>
+using std::less;
 using std::vector;
 
 void insertionSort(vector<int> & nums);
 void insertionSort(vector<int> & nums, int i, int j);
+
+template <typename Iterator, typename Comperator>
+void insertionSort(const Iterator& begin, const Iterator& end, \
+					Comperator lessThan);
+template <typename Iterator>
+void insertionSort(const Iterator& begin, const Iterator& end);
 
 void insertionSort(vector<int> & nums)
 {
@@ -22,10 +30,33 @@ void insertionSort(vector<int> & nums, int lo, int hi)
 	}
 }
 
+template <typename Iterator>
+void insertionSort(const Iterator& begin, const Iterator& end)
+{
+	insertionSort(begin, end, less<decltype(*begin)>{});
+}
+
+template <typename Iterator, typename Comperator>
+void insertionSort(const Iterator& begin, const Iterator& end, \
+					Comperator lessThan)
+{
+	if (begin == end)
+		return;
+	Iterator j;
+	for (Iterator p = begin + 1; p != end; ++p)
+	{
+		auto tmp = std::move(*p);
+		for (j = p; j != begin && lessThan(tmp, *(j - 1)); --j)
+			*j = std::move(*(j - 1));
+		*j = std::move(tmp);
+	}
+}
+
+
 int main()
 {
 	vector<int> nums { 2, 1, 4, 7, 4, 8, 3, 6, 4, 8 };
-	insertionSort(nums);
+	insertionSort(nums.begin(), nums.end());
 	for (int num : nums)
 		std::cout << num << std::endl;
 	return 0;
