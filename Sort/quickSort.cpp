375564f2f2640c1f501bf5b710f0c338bd3a812e
csyang79 <csyang79@163.com>
2019-10-03 00:49:49 +0800

    update quickSort

--- Sort/quickSort.cpp
+++ Sort/quickSort.cpp
@@ -2,68 +2,39 @@
 #include <vector>
 using std::vector;
 
-int median3(vector<int> & nums, int start, int end);
-void insertionSort(vector<int>& nums, int left, int right);
-void quickSort(vector<int> & nums);
-void quickSort(vector<int> & nums, int start, int end);
-void quickSort(vector<int> & nums)
+void quickSort(vector<int>& nums, int left, int right);
+void quickSort(vector<int>& nums);
+int partation(vector<int>& nums, int left, int right);
+
+
+int partation(vector<int>& nums, int left, int right)
+{
+	int t = nums[right], k = left;
+	for (int i = left; i < right; ++i)
+		if (nums[i] < t)
+			std::swap(nums[i], nums[k++]);
+	std::swap(nums[k], nums[right]);
+	return k;
+}
+
+void quickSort(vector<int>& nums, int left, int right)
+{
+	if (left < right)
+	{
+		int pivot = partation(nums, left, right);
+		quickSort(nums, left, pivot - 1);
+		quickSort(nums, pivot + 1, right);
+	}
+}
+void quickSort(vector<int>& nums)
 {
 	quickSort(nums, 0, nums.size() - 1);
 }
 
-
-void quickSort(vector<int> & nums, int left, int right)
-{
-	if (left + 10 < right)
-	{
-		int pivot = median3(nums, left, right);
-		int i = left, j = right - 1;
-		while (1)
-		{
-			while (nums[++i] < pivot);
-			while (nums[--j] > pivot);
-			if (i < j)
-				std::swap(nums[i], nums[j]);
-		}
-		std::swap(nums[right - 1], nums[i]);
-		quickSort(nums, left, i - 1);
-		quickSort(nums, i + 1, right); 
-	}
-	else
-	{
-		insertionSort(nums, left, right);
-	}
-}
-void insertionSort(vector<int>& nums, int left, int right)
-{
-	for (int i = left + 1; i <= right; ++i)
-	{
-		int t = std::move(nums[i]);
-		int j;
-		for (j = i - 1; j >= 0 && nums[j] > t; --j)
-			nums[j + 1] = std::move(nums[j]);
-		nums[j + 1] = std::move(t);	
-	}
-}
-
-int median3(vector<int> & nums, int start, int end)
-{
-	int mid = start + (end - start) >> 1;
-	if (nums[start] > nums[mid])
-		std::swap(nums[start], nums[mid]);
-	if (nums[mid] > nums[end])
-		std::swap(nums[mid], nums[end]);
-	if (nums[start] > nums[mid])
-		std::swap(nums[start], nums[mid]);
-	std::swap(nums[mid], nums[end - 1]);
-	return nums[end - 1];
-}
 int main()
 {
-	vector<int> nums{2, 1, 4, 7 ,4, 8, 3, 6, 4, 8};
-	std::cout << nums.size() << std::endl;
+	vector<int> nums { 2, 1, 4, 7, 4, 8, 3, 6, 4, 7 };
 	quickSort(nums);
-//	insertionSort(nums, 0, nums.size() - 1);
 	for (int num : nums)
 		std::cout << num << std::endl;
 	return 0;
